987bd0926c83ab39320098e735606df28368bcfb
cheymos <nevesomov@gmail.com>
2021-10-27 20:28:57 +0600

    Connect typeorm module and add examples of config files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import ormConfig from './configs/orm.config';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(ormConfig)],
 })
 export class AppModule {}
