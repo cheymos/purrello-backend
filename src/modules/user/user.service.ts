20afa4b34614d672912b21d54bb756687aa47d06
cheymos <nevesomov@gmail.com>
2021-10-27 21:21:45 +0600

    Implements create and find methods in user service

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { RegisterDto } from '../dtos/register.dto';
+import { UserEntity } from './entities/user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  async create({
+    username,
+    email,
+    password,
+  }: RegisterDto): Promise<UserEntity> {
+    const newUser = new UserEntity(username, email, password);
+
+    return this.userRepository.save(newUser);
+  }
+
+  findBy(
+    property: keyof UserEntity,
+    value: string | number,
+    withPassword?: boolean,
+  ): Promise<UserEntity | undefined> {
+    const selectedFields: (keyof UserEntity)[] = [
+      'id',
+      'username',
+      'email',
+      'role',
+    ];
+
+    if (withPassword) {
+      selectedFields.push('password');
+    }
+
+    return this.userRepository.findOne(
+      { [property]: value },
+      { select: selectedFields },
+    );
+  }
+}
