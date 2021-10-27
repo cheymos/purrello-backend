e303cc63338a7926c7127a89a2342266a9c5a98e
cheymos <nevesomov@gmail.com>
2021-10-28 01:13:24 +0600

    Create auth middleware
    This middleware adds the user's payload(userPayload) to the request.

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { RefreshTokenEntity } from './entities/refresh-token.entity';
+import { AuthMiddleware } from './middlewares/auth.middleware';
 import { TokenService } from './token.service';
 
 @Module({
@@ -14,4 +15,8 @@ import { TokenService } from './token.service';
   controllers: [AuthController],
   providers: [AuthService, TokenService],
 })
-export class AuthModule {}
+export class AuthModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes('*');
+  }
+}
