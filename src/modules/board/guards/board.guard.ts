c0f61e0ce5507d2725cdae4ac4767d3455c3f091
cheymos <nevesomov@gmail.com>
2021-10-28 20:53:11 +0600

    Improve board guard: added ownership check

--- src/modules/board/guards/board.guard.ts
+++ src/modules/board/guards/board.guard.ts
@@ -1,13 +1,18 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable
+} from '@nestjs/common';
+import { ACCESS_DENIED } from '../../../common/constants/error.constants';
 import { ModifyExpressRequest } from '../../../common/types/modify-express-request.type';
 import { BoardService } from '../board.service';
+import { BoardEntity } from '../entities/board.entity';
 
 @Injectable()
 export class BoardGuard implements CanActivate {
   constructor(private readonly boardService: BoardService) {}
-  canActivate(
-    context: ExecutionContext,
-  ): Promise<boolean> {
+  canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest<ModifyExpressRequest>();
 
     return this.validateRequest(request);
@@ -17,6 +22,15 @@ export class BoardGuard implements CanActivate {
     const boardId = +request.params.boardId;
     const board = await this.boardService.findById(boardId);
 
-    return Boolean(board);
+    const userId = request.userPayload?.id;
+
+    if (userId && !this.checkAccess(userId, board))
+      throw new ForbiddenException(ACCESS_DENIED);
+
+    return true;
+  }
+
+  checkAccess(userId: number, board: BoardEntity): boolean {
+    return board.ownerId === userId;
   }
 }
