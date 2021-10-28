d91ef01045ae996a6d576b4701bf3a73f17bfc39
cheymos <nevesomov@gmail.com>
2021-10-28 17:24:00 +0600

    Create empty board module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import ormConfig from './configs/orm.config';
 import { AuthModule } from './modules/auth/auth.module';
+import { BoardModule } from './modules/board/board.module';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormConfig), UserModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot(ormConfig),
+    UserModule,
+    AuthModule,
+    BoardModule,
+  ],
 })
 export class AppModule {}
