fcf661031bdcebad70dfa22dd20933a341082a5d
cheymos <nevesomov@gmail.com>
2021-10-28 18:26:16 +0600

    Add board dto

---
+++ src/modules/board/dtos/board.dto.ts
@@ -0,0 +1,11 @@
+import { IsBoolean, IsOptional, IsString, Length } from 'class-validator';
+
+export class BoardDto {
+  @IsString()
+  @Length(1, 255)
+  title: string;
+
+  @IsOptional()
+  @IsBoolean()
+  isPrivate: boolean;
+}
