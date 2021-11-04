34fb610fcd360ca681b61a8e78ec651932e17b2c
cheymos <nevesomov@gmail.com>
2021-11-05 04:32:02 +0600

    Remove unnecessary examples and add config files

--- src/configs/jwt.config.ts
+++ src/configs/jwt.config.ts
@@ -1,8 +1,5 @@
 import { SignOptions } from 'jsonwebtoken';
 
-export const JWT_ACCESS_SECRET = 'YOUR_SECRET_KEY';
-export const JWT_REFRESH_SECRET = 'YOUR_SECRET_KEY';
-
 export const jwtAccessConfig: SignOptions = {
   expiresIn: '30m',
 };
